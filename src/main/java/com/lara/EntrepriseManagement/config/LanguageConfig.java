550aa94a142f2e4a92cdb1962dc8bae8ae8b4964
medisensei <medios1596@gmail.com>
2024-09-11 22:38:44 +0100

    fix name

--- src/main/java/com/lara/EntrepriseManagement/config/LanguageConfig.java
+++ src/main/java/com/lara/EntrepriseManagement/config/LanguageConfig.java
@@ -1,14 +1,14 @@
 package com.lara.EntrepriseManagement.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 
 import java.util.Locale;
 
-@org.springframework.context.annotation.Configuration
+@Configuration
 public class LanguageConfig {
-
 
 
     @Bean
